22417f16be08ac49af671c256484a57b7f05e4d3
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-09 16:08:36 +0100

    Update payload.go

--- payload.go
+++ payload.go
@@ -1,281 +1,138 @@
 package main
+
 import (
-	"bytes"
-	"compress/bzip2"
-	"crypto/sha256"
-	"encoding/binary"
-	"encoding/hex"
-	"errors"
+	"archive/zip"
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"os"
-	"sort"
-	"sync"
+	"runtime"
+	"strings"
 	"time"
-
-
-	humanize "github.com/dustin/go-humanize"
-	"github.com/golang/protobuf/proto"
-	xz "github.com/spencercw/go-xz"
-	"github.com/ssut/payload-dumper-go/chromeos_update_engine"
-	"github.com/vbauerster/mpb/v5"
-	"github.com/vbauerster/mpb/v5/decor"
 )
 
-type request struct {
-	partition       *chromeos_update_engine.PartitionUpdate
-	targetDirectory string
-}
-
-// Payload is a new format for the Android OTA/Firmware update files since Android Oreo
-type Payload struct {
-	Filename string
-
-	file                 *os.File
-	header               *payloadHeader
-	deltaArchiveManifest *chromeos_update_engine.DeltaArchiveManifest
-	signatures           *chromeos_update_engine.Signatures
-
-	concurrency int
-
-	metadataSize int64
-	dataOffset   int64
-	initialized  bool
-
-	requests chan *request
-	workerWG sync.WaitGroup
-	progress *mpb.Progress
-}
-
-const payloadHeaderMagic = "CrAU"
-const brilloMajorPayloadVersion = 2
-const blockSize = 4096
-
-type payloadHeader struct {
-	Version              uint64
-	ManifestLen          uint64
-	MetadataSignatureLen uint32
-	Size                 uint64
-
-	payload *Payload
-}
-
-func (ph *payloadHeader) ReadFromPayload() error {
-	buf := make([]byte, 4)
-	if _, err := ph.payload.file.Read(buf); err != nil {
-		return err
+func extractPayloadBin(filename string) string {
+	zipReader, err := zip.OpenReader(filename
+			if err != nil {
+		log.Fatalf("Not a valid zip archive: %s\n", filename)
 	}
-	if string(buf) != payloadHeaderMagic {
-		return fmt.Errorf("Invalid payload magic: %s", buf)
-	}
+	defer zipReader.Close()
 
-	// Read Version
-	buf = make([]byte, 8)
-	if _, err := ph.payload.file.Read(buf); err != nil {
-		return err
-	}
-	ph.Version = binary.BigEndian.Uint64(buf)
-	fmt.Printf("Payload Version: %d\n", ph.Version)
+	for _, file := range zipReader.Reader.File {
+		if file.Name == "payload.bin" && file.UncompressedSize64 > 0 {
+			zippedFile, err := file.Open()
+			if err != nil {
+				log.Fatalf("Failed to read zipped file: %s\n", file.Name)
+			}
 
-	if ph.Version != brilloMajorPayloadVersion {
-		return fmt.Errorf("Unsupported payload version: %d", ph.Version)
-	}
+			tempfile, err := ioutil.TempFile(os.TempDir(), "payload_*.bin")
+			if err != nil {
+				log.Fatalf("Failed to create a temp file located at %s\n", tempfile.Name())
+			}
+			defer tempfile.Close()
 
-	// Read Manifest Len
-	buf = make([]byte, 8)
-	if _, err := ph.payload.file.Read(buf); err != nil {
-		return err
-	}
-	ph.ManifestLen = binary.BigEndian.Uint64(buf)
-	fmt.Printf("Payload Manifest Length: %d\n", ph.ManifestLen)
+			_, err = io.Copy(tempfile, zippedFile)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	ph.Size = 24
-
-	// Read Manifest Signature Length
-	buf = make([]byte, 4)
-	if _, err := ph.payload.file.Read(buf); err != nil {
-		return err
-	}
-	ph.MetadataSignatureLen = binary.BigEndian.Uint32(buf)
-	fmt.Printf("Payload Manifest Signature Length: %d\n", ph.MetadataSignatureLen)
-
-	return nil
-}
-
-// NewPayload creates a new Payload struct
-func NewPayload(filename string) Payload {
-	payload := Payload{
-		Filename:    filename,
-		concurrency: 4,
-	}
-
-	return payload
-}
-
-// SetConcurrency sets number of workers
-func (p *Payload) SetConcurrency(concurrency int) {
-	p.concurrency = concurrency
-}
-
-// GetConcurrency returns number of workers
-func (p *Payload) GetConcurrency() int {
-	return p.concurrency
-}
-
-//Repack repacks payload.bin
-func (p *Payload) Repack(targetDirectory string) error {
-	if err := p.Open(); err != nil {
-		return err
-	}
-	defer p.file.Close()
-
-	if err := p.readHeader(); err != nil {
-		return err
-//creates tries to open payload.bin file defined by Filename
-func (p *Payload) Open() error {
-	if p.file != nil {
-		return nil
-	}
-
-	file, err := os.Open(p.Filename)
-	if err != nil {
-		return err
-	}
-	p.file = file
-
-	return nil
-}
-
-func (p *Payload) readHeader() error {
-	if err := p.header.ReadFromPayload(); err != nil {
-		return err
-	}
-
-	return nil
-}
-func (p *Payload) readMetadataSignature() (*chromeos_update_engine.Signatures, error) {
-	if _, err := p.file.Seek(int64(p.header.Size+p.header.ManifestLen), 0); err != nil {
-		return nil, err
-	}
-
-	buf := make([]byte, p.header.MetadataSignatureLen)
-	if _, err := p.file.Read(buf); err != nil {
-		return nil, err
-	}
-	signatures := &chromeos_update_engine.Signatures{}
-	if err := proto.Unmarshal(buf, signatures); err != nil {
-		return nil, err
-	}
-
-	return signatures, nil
-}
-
-func (p *Payload) Init() error {
-	// Read Header
-	p.header = &payloadHeader{
-		payload: p,
-	}
-	if err := p.header.ReadFromPayload(); err != nil {
-		return err
-	}
-
-	// Read Manifest
-	deltaArchiveManifest, err := p.readManifest()
-	if err != nil {
-		return err
-	}
-	p.deltaArchiveManifest = deltaArchiveManifest
-
-	// Read Signatures
-	signatures, err := p.readMetadataSignature()
-	if err != nil {
-		return err
-	}
-	p.signatures = signatures
-
-	// Update sizes
-	p.metadataSize = int64(p.header.Size + p.header.ManifestLen)
-	p.dataOffset = p.metadataSize + int64(p.header.MetadataSignatureLen)
-
-	fmt.Println("Found partitions:")
-	for i, partition := range p.deltaArchiveManifest.Partitions {
-		fmt.Printf("%s (%s)", partition.GetPartitionName(), humanize.Bytes(*partition.GetNewPartitionInfo().Size))
-
-		if i < len(deltaArchiveManifest.Partitions)-1 {
-			fmt.Printf(", ")
-		} else {
-			fmt.Printf("\n")
+			return tempfile.Name()
 		}
 	}
-	p.initialized = true
 
-	return nil
+	return ""
 }
 
-func (p *Payload) readDataBlob(offset int64, length int64) ([]byte, error) {
-	buf := make([]byte, length)
-	n, err := p.file.ReadAt(buf, p.dataOffset+offset)
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var (
+		list            bool
+		partitions      string
+		outputDirectory string
+		concurrency     int
+	)
+
+	flag.IntVar(&concurrency, "c", 4, "Number of multiple workers to extract (shorthand)")
+	flag.IntVar(&concurrency, "concurrency", 4, "Number of multiple workers to extract")
+	flag.BoolVar(&list, "l", false, "Show list of partitions in payload.bin (shorthand)")
+	flag.BoolVar(&list, "list", false, "Show list of partitions in payload.bin")
+	flag.StringVar(&outputDirectory, "o", "", "Set output directory (shorthand)")
+	flag.StringVar(&outputDirectory, "output", "", "Set output directory")
+	flag.StringVar(&partitions, "p", "", "Set partitions to extract (shorthand)")
+	flag.StringVar(&partitions, "partitions", "", "Set partitions to extract")
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if flag.NArg() == 0 {
+		log.Fatal("Please specify a payload.bin file")
+	}
+
+	if flag.NArg() > 1 {
+		log.Fatal("Please specify only one payload.bin file")
+	}
+
+	if outputDirectory == "" {
+		log.Fatal("Please specify an output directory")
+	}
+
+	if _, err := os.Stat(outputDirectory); os.IsNotExist(err) {
+		os.Mkdir(outputDirectory, 0755)
+	}
+
+	if partitions == "" {
+		log.Fatal("Please specify partitions to extract")
+	}
+
+	if concurrency < 1 {
+		log.Fatal("Please specify a concurrency greater than 0")
+	}
+
+	payloadFile := flag.Arg(0)
+	if _, err := os.Stat(payload
+		log.Fatal(err)
+	}
+
+	payloadBin := extractPayloadBin(payloadFile)
+	if payloadBin == "" {
+		log.Fatalf("Failed to extract payload.bin from %s\n", payloadFile)
+	}
+
+	defer os.Remove(payloadBin)
+
+	payloadReader, err := NewReader(payloadBin, 0)
 	if err != nil {
-		return nil, err
-	}
-	if int64(n) != length {
-		return nil, fmt.Errorf("Read length mismatch: %d != %d", n, length)
+		log.Fatal(err)
 	}
 
-	return buf, nil
-}
+	defer payloadReader.Close()
 
-func (p *Payload) Repack(partition *chromeos_update_engine.PartitionUpdate, out *os.File) error {
-	// Write data blob
-	buf, err := p.readDataBlob(*partition.DataOffset, *partition.DataLength)
-	if err != nil {
-		return err
-	}
-	if _, err := out.Write(buf); err != nil {
-		return err
+	if list {
+		for _, partition := range payloadReader.Partitions {
+			fmt.Println(partition.Name)
+		}
+		os.Exit(0)
 	}
 
-	return nil
-}
-
-func (p *Payload) spawnExtractWorkers(n int) {
-	for i := 0; i < n; i++ {
-		go p.worker()
-	}
-}
-
-func (p *Payload) ExtractSelected(targetDirectory string, partitions []string) error {
-	if !p.initialized {
-		return errors.New("Payload has not been initialized")
-	}
-	p.progress = mpb.New()
-
-	p.requests = make(chan *request, 100)
-	p.spawnExtractWorkers(p.concurrency)
-
-	sort.Strings(partitions)
-
-	for _, partition := range p.deltaArchiveManifest.Partitions {
-		if len(partitions) > 0 {
-			idx := sort.SearchStrings(partitions, *partition.PartitionName)
-			if idx == len(partitions) || partitions[idx] != *partition.PartitionName {
-				continue
+	partitionsToExtract := strings.Split(partitions, ",")
+	for _, partitionName := range partitionsToExtract {
+		partitionFound := false
+		for _, partition := range payloadReader.Partitions {
+			if partition.Name == partitionName {
+				partitionFound = true
+				break
 			}
 		}
 
-		p.workerWG.Add(1)
-		p.requests <- &request{
-			partition:       partition,
-			targetDirectory: targetDirectory,
+		if !partitionFound {
+			log.Fatalf("Partition %s not found in payload.bin\n", partitionName)
 		}
 	}
 
-	p.workerWG.Wait()
-	close(p.requests)
-
-	return nil
-}
-
-func (p *Payload) ExtractAll(targetDirectory string) error {
-	return p.ExtractSelected(targetDirectory, nil)
-}
+	
