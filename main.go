89109c65f8731fb1e679df03000a8ac5c7cdfdd8
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-09 16:24:28 +0100

    Update main.go

--- main.go
+++ main.go
@@ -45,11 +45,17 @@ func extractPayloadBin(filename string) string {
 	return ""
 }
 
+func usage() {
+	fmt.Printf("Usage: %s [options] payload.bin.zip
+
+", os.Args[0])
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	var (
 		list            bool
+		repack      bool
 		partitions      string
 		outputDirectory string
 		concurrency     int
@@ -57,8 +63,10 @@ func main() {
 
 	flag.IntVar(&concurrency, "c", 4, "Number of multiple workers to extract (shorthand)")
 	flag.IntVar(&concurrency, "concurrency", 4, "Number of multiple workers to extract")
-	flag.BoolVar(&list, "l", false, "Show list of partitions in payload.bin (shorthand)")
-	flag.BoolVar(&list, "list", false, "Show list of partitions in payload.bin")
+	flag.BoolVar(&list, "l", false, "List partitions (shorthand)")
+	flag.BoolVar(&list, "list", false, "List partitions")
+	flag.BoolVar(&repack, "r", false, "Repack payload.bin (shorthand)")
+	flag.BoolVar(&repack, "repack", false, "Repack payload.bin")
 	flag.StringVar(&outputDirectory, "o", "", "Set output directory (shorthand)")
 	flag.StringVar(&outputDirectory, "output", "", "Set output directory")
 	flag.StringVar(&partitions, "p", "", "Dump only selected partitions (comma-separated) (shorthand)")
